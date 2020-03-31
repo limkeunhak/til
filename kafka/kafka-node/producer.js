a5c0224dec68558413d638beb4d7d76a95508c7f
Keunhak Lim <limkeunhak@gmail.com>
2020-03-31 22:02:17 +0900

    kafka study

--- kafka/kafka-node/producer.js
+++ kafka/kafka-node/producer.js
@@ -1,22 +1,42 @@
 var kafka = require("kafka-node"),
     Producer = kafka.Producer,
-    client = new kafka.KafkaClient({ kafkaHost: '127.0.0.1:9092' }),
+    client = new kafka.KafkaClient({ kafkaHost: 'localhost:9092' }),
     producer = new Producer(client);
 
 let count = 0;
 
+/*
+let topicsToCreate = [{
+  topic: 'cat',
+  partitions: 1,
+  replicationFactor: 1
+}];
+
+client.createTopics(topicsToCreate, (err, result) => {
+	console.log('createTopics');
+  // result is an array of any errors if a given topic could not be created
+	if (err) {
+			console.log(err);
+	} else {
+			console.log(result);
+	}
+});
+*/
 producer.on("ready", function () {
-    console.log("ready");
+		console.log('ready');
     setInterval(function () {
-        payloads = [
-            { topic: "cat", messages: `I have ${count} cats`, partition: 0 }
+        let payloads = [
+            { topic: "cat", messages: `I have ${count} cats` }
         ];
 
         producer.send(payloads, function (err, data) {
-            console.log(data);
+					  if (err) {
+								console.log(err);
+						} else {
+								console.log(data);
+						}
             count += 1;
         });
-        console.log(payloads);
     }, 5000);
 });
 
